a06bafb01eb2523fe606e756c349263e81dc0ad7
Isaac Obuya <Szack4477@gmail.com>
2022-09-28 18:17:13 +0300

    Explore Spring MCV Controllers and Views

---
+++ src/main/java/com/springmvc/conference/configs/ConferenceConfig.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.springmvc.conference.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author Zakol
+ */
+@Configuration
+public class ConferenceConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("/WEB-INF/pdf/");
+    }
+    
+}
